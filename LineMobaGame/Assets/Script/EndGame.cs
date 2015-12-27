3b9fa8725c05079216ba6f893e75f1e0ad6dbf60
andrey.zaycev <dondrakone89@mail.ru>
2015-12-27 23:43:13 +0300

    f43ff

---
+++ LineMobaGame/Assets/Script/EndGame.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGame : MonoBehaviour {
+
+    KTpoint blue;
+    KTpoint red;
+
+    void Start()
+    {
+        blue = GameObject.Find("KT1").GetComponent<KTpoint>();
+        red = GameObject.Find("KT2").GetComponent<KTpoint>();
+
+    }
+
+   public void victory()
+    {
+        Time.timeScale = 0.2f;
+    }
+    public void defeat()
+    {
+        Time.timeScale = 0.2f;
+    }
+
+}
